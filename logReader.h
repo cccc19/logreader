f4c8a59877e0e461988cbcffb4e562085248d7ba
stealyglint <pdawson@PhysicsRT.cco>
2020-05-02 12:40:57 +0100

    Fix Aria server address

--- logReader.h
+++ logReader.h
@@ -20,7 +20,7 @@ class logReader
 
 
     private:
-        std::string basedir = "\\\\aria-file01\\Va_Transfer\\TDS\\";
+        std::string basedir = "\\\\aria-file01.cccaria.local\\Va_Transfer\\TDS\\";
         int MAX_MACHINES;
         struct machInfoType {
             std::string MachSerNo;
